3159dbaeba5a768fc9c91482f953166e7fbe9b3a
Md Sakib hossen <akibhossainsakib1040@gmail.com>
2025-09-08 02:19:09 +0600

    dynamic card with the dynamic card design

--- js/script.js
+++ js/script.js
@@ -16,10 +16,10 @@ const displayLessons = (lessons) => {
   //? ----> Get  Every lessons Each
   lessons.forEach((lesson) => {
     //?-----> Create Elements
-    console.log(lesson.level_no);
+    // console.log(lesson.level_no);
     const btnDiv = document.createElement("div");
     btnDiv.innerHTML = `
-    <button class="btn btn-outline btn-primary">
+    <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
     <i class="fa-solid fa-book-open"></i>Lesson - ${lesson.level_no}
     </button>
     `;
@@ -27,6 +27,33 @@ const displayLessons = (lessons) => {
     // console.log(lesson);
     //?---> Appending into the content
     levelContainer.append(btnDiv);
+  });
+};
+
+const loadLevelWord = (id) => {
+  const url = `https://openapi.programming-hero.com/api/level/${id}`;
+  //// console.log(id);
+  //// console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((words) => displayLevelWord(words.data));
+};
+
+const displayLevelWord = (words) => {
+  //? ---> Getting the container for storing the values and emptying it
+  const wordContainer = document.getElementById("word_container");
+  wordContainer.innerHTML = "";
+
+  //?--->Looping through the array of object for getting every one of them
+  words.forEach((word) => {
+    //?Creating an element for storing that value
+    const boxDiv = document.createElement("div");
+    boxDiv.innerHTML = `
+    <p> Cat </p>
+    `;
+
+    //?Appending the child to the parent container
+    wordContainer.appendChild(boxDiv);
   });
 };
 
